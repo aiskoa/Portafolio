9b10ec2f74c24ae49c8f866fd2f174e5736b00ac
Rawier <alexam211@hotmail.com>
2024-11-12 22:35:55 -0600

    Cambios en blog

--- components/section/post/index.tsx
+++ components/section/post/index.tsx
@@ -7,24 +7,24 @@ export default function Post({ post }: PostType) {
   const { t } = useTranslation("index");
   return (
     <Link href={`/blog/${post.slug}`}>
-      <div className="w-full px-4 my-4 md:px-6 lg:w-1/3">
+      <div className="w-full px-4 my-4 custom-card md:px-6 lg:px-8">
         <article className="overflow-hidden border-2 rounded-lg shadow-lg dark:hover:border-violet-700 hover:border-black dark:bg-slate-800">
           <a href={`/blog/${post.slug}`}>
             <img
               alt={post.frontmatter.title}
-              className="block object-cover w-full h-48" // Mantén la imagen en la parte superior
+              className="block object-cover w-full custom-image" 
               src={post.frontmatter.cover_image}
             />
           </a>
           <div className="p-4">
             <header className="leading-tight">
-              <h1 className="text-xl font-bold">
+              <h1 className="text-2xl font-bold">
                 <a href={`/blog/${post.slug}`} className="hover:underline dark:text-white">
                   {post.frontmatter.title}
                 </a>
               </h1>
               <p className="mt-2 text-gray-600 dark:text-gray-300">
-                {post.frontmatter.excerpt} {/* Assuming you have an excerpt or short description */}
+                {post.frontmatter.excerpt} 
               </p>
               <div className="flex items-center mt-2">
                 <button title="Badge" className="flex items-center px-2 py-1 mr-2 text-sm font-semibold border-2 rounded-md dark:border-b-violet-300 dark:bg-zinc-900 border-b-zinc-500 border-l-zinc-400">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
     extend: {
+      width: {
+        'custom-card': '350px',  // Define el ancho personalizado para las tarjetas
+      },
+      height: {
+        'custom-image': '200px', // Define la altura personalizada para las imágenes
+      },
       backgroundImage: {
         'tv': "url('https://i.makeagif.com/media/4-05-2022/22uZbY.gif')",
       },
@@ -22,4 +28,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
